4f7bef65d7cc18f1f9849b1ee3d8216505e3a97e
yoshino9397 <yayamayoshino@gmail.com>
2021-12-19 18:43:07 -0800

    fin

--- 01-birthday-reminder/setup/src/data.js
+++ 01-birthday-reminder/setup/src/data.js
@@ -1,22 +1,22 @@
 export default [
   {
     id: 1,
-    name: 'Bertie Yates',
-    age: 29,
+    name: 'Yoshino Yayama',
+    age: 5,
     image:
       'https://res.cloudinary.com/diqqf3eq2/image/upload/v1595959131/person-2_ipcjws.jpg',
   },
   {
     id: 2,
-    name: 'Hester Hogan',
-    age: 32,
+    name: 'Kaori',
+    age: 3,
     image:
       'https://res.cloudinary.com/diqqf3eq2/image/upload/v1595959131/person-3_rxtqvi.jpg',
   },
   {
     id: 3,
-    name: 'Larry Little',
-    age: 36,
+    name: 'Yuki',
+    age: 1,
     image:
       'https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883423/person-4_t9nxjt.jpg',
   },
