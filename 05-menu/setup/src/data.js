241c2868021bb51a73d837e776eceb920781352c
yoshino9397 <yayamayoshino@gmail.com>
2021-12-27 23:12:34 -0800

    fin

--- 05-menu/setup/src/data.js
+++ 05-menu/setup/src/data.js
@@ -71,5 +71,13 @@ const menu = [
     img: './images/item-9.jpeg',
     desc: `skateboard fam synth authentic semiotics. Live-edge lyft af, edison bulb yuccie crucifix microdosing.`,
   },
+  {
+    id: 10,
+    title: 'Sushi',
+    category: 'dinner',
+    price: 99.99,
+    img: './images/item-6.jpeg',
+    desc: `sushi ga totemo kuitai.aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa`,
+  },
 ];
 export default menu;
