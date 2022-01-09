9da328759080f65f8df719d3fe512c91ea599ea1
yoshino9397 <yayamayoshino@gmail.com>
2022-01-08 23:51:28 -0800

    fin

--- 01-birthday-reminder/setup/src/List.js
+++ 01-birthday-reminder/setup/src/List.js
@@ -1,18 +1,21 @@
-import React from 'react';
+import React from "react";
 
-const List = ({people}) => {
+const List = ({ people }) => {
   return (
     <>
-     {people.map((person)=>{
-      const{id,name,age,image} =person;
-      return <article key={id} className='person'>
-        <img src={image} alt={name}/>
-        <div>
-          <h4>{name}</h4>
-          <p>{age}years</p>
-        </div>
-      </article>
-     })}
+      {people.map((person) => {
+        const { id, name, age, image } = person;
+        return (
+          <article key={id} className="person">
+            <img src={image} alt={name} />
+            <div>
+              <h4>{name}</h4>
+              <p>{age}years</p>
+            </div>
+          </article>
+          //////空のタグはreturnの中にエレメントがないのでそのエラー予防//////////
+        );
+      })}
     </>
   );
 };
