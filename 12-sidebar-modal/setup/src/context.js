bf11883e04f4bca4d8e51f4d50128351952352e8
矢山佳乃 <yayamayoshino@yayamayoshinonoMacBook-Air.local>
2022-01-15 02:04:42 -0800

    anyway

--- 12-sidebar-modal/setup/src/context.js
+++ 12-sidebar-modal/setup/src/context.js
@@ -1,3 +1,46 @@
 import React, { useState, useContext } from "react";
+///useContextをインポート
 
 const AppContext = React.createContext()
+///①親コンポーネントから子コンポーネントにデータを渡す処理propsを使わずにできるので煩雑さが減る
+
+const AppProvider=()=>{
+
+return(
+<AppContext.Provider value={{}}>
+
+</AppContext.Provider>
+///②.providerのタグで囲むのは必須。これで親コンポーネントの設定完了
+
+
+
+)
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+export const useGlobalContext = () => {
+  return useContext(AppContext);
+};
+///作成したUserCountをexportしているのはContextを利用するComponentCでimportを行うため
