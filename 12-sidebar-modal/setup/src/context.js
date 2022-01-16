a2265130427dde34cee71655792db30735dea0dd
矢山佳乃 <yayamayoshino@yayamayoshinonoMacBook-Air.local>
2022-01-15 23:02:29 -0800

    context

--- 12-sidebar-modal/setup/src/context.js
+++ 12-sidebar-modal/setup/src/context.js
@@ -1,46 +1,51 @@
 import React, { useState, useContext } from "react";
 ///useContextをインポート
 
-const AppContext = React.createContext()
+const AppContext = React.createContext();
 ///①親コンポーネントから子コンポーネントにデータを渡す処理propsを使わずにできるので煩雑さが減る
 
-const AppProvider=()=>{
+const AppProvider = () => {
+  const [isSidebarOpen, setIsSidebarOpen] = useState(false);
+  const [isModalOpen, setIsModalOpen] = useState(false);
 
-return(
-<AppContext.Provider value={{}}>
+  const openSidebar = () => {
+    setIsSidebarOpen(true);
+  };
 
-</AppContext.Provider>
-///②.providerのタグで囲むのは必須。これで親コンポーネントの設定完了
+  const closeSidebar = () => {
+    setIsSidebarOpen(false);
+  };
 
+  const openModal = () => {
+    setIsModalOpen(true);
+  };
 
+  const closeModal = () => {
+    setIsModalOpen(false);
+  };
 
-)
-
-}
+  return (
+    <AppContext.Provider
+      value={{
+        isSidebarOpen,
+        isModalOpen,
+        openModal,
+        closeModal,
+        openSidebar,
+        closeSidebar,
+      }}
+    >
+      {children}
+    </AppContext.Provider>
+    ///②.providerのタグで囲むのは必須。これで親コンポーネントの設定完了
+  );
+};
 
 ///https://reffect.co.jp/react/react-usecontext-understanding#useContext
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 export const useGlobalContext = () => {
   return useContext(AppContext);
 };
 ///作成したUserCountをexportしているのはContextを利用するComponentCでimportを行うため
+
+export { AppContext, AppProvider };
