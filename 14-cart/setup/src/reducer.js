7d587d940fdfef40eac6947fda5452cb6727a3b1
矢山佳乃 <yayamayoshino@yayamayoshinonoMacBook-Air.local>
2022-01-20 21:58:35 -0800

    fin

--- 14-cart/setup/src/reducer.js
+++ 14-cart/setup/src/reducer.js
@@ -1,6 +1,6 @@
 const reducer = (state, action) => {
   if (action.type === "CLEAR_CART") {
-    return { ...state, car: [] };
+    return { ...state, cart: [] };
   }
   if (action.type === "REMOVE") {
     return {
@@ -11,10 +11,7 @@ const reducer = (state, action) => {
   if (action.type === "INCREASE") {
     let tempCart = state.cart.map((cartItem) => {
       if (cartItem.id === action.payload) {
-        return {
-          ...cartItem,
-          amount: cartItem.amount + 1,
-        };
+        return { ...cartItem, amount: cartItem.amount + 1 };
       }
       return cartItem;
     });
@@ -24,10 +21,7 @@ const reducer = (state, action) => {
     let tempCart = state.cart
       .map((cartItem) => {
         if (cartItem.id === action.payload) {
-          return {
-            ...cartItem,
-            amount: cartItem.amount - 1,
-          };
+          return { ...cartItem, amount: cartItem.amount - 1 };
         }
         return cartItem;
       })
@@ -50,6 +44,32 @@ const reducer = (state, action) => {
       }
     );
     total = parseFloat(total.toFixed(2));
+
     return { ...state, total, amount };
   }
+  if (action.type === "LOADING") {
+    return { ...state, loading: true };
+  }
+  if (action.type === "DISPLAY_ITEMS") {
+    return { ...state, cart: action.payload, loading: false };
+  }
+  if (action.type === "TOGGLE_AMOUNT") {
+    let tempCart = state.cart
+      .map((cartItem) => {
+        if (cartItem.id === action.payload.id) {
+          if (action.payload.type === "inc") {
+            return { ...cartItem, amount: cartItem.amount + 1 };
+          }
+          if (action.payload.type === "dec") {
+            return { ...cartItem, amount: cartItem.amount - 1 };
+          }
+        }
+        return cartItem;
+      })
+      .filter((cartItem) => cartItem.amount !== 0);
+    return { ...state, cart: tempCart };
+  }
+  throw new Error("no matching action type");
 };
+
+export default reducer;
