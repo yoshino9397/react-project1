8df79797df41dcc321b076bfc0c768496749b820
矢山佳乃 <yayamayoshino@yayamayoshinonoMacBook-Air.local>
2022-02-19 18:49:32 -0800

    anyway

--- 20-dark-mode/setup/src/App.js
+++ 20-dark-mode/setup/src/App.js
@@ -1,9 +1,34 @@
-import React, { useState, useEffect } from 'react'
-import data from './data'
-import Article from './Article'
+import React, { useState, useEffect } from "react";
+import data from "./data";
+import Article from "./Article";
 
-function App() {
-  return <h2>dark mode starter</h2>
+const getStorageTheme=()=>{
+  let theme ='light-theme'
+  if (localStorage.getItem) {
+    
+  }
 }
 
-export default App
+
+
+function App() {
+  return (
+    <main>
+      <nav>
+        <div className="nav-center">
+          <h1>overreacted</h1>
+          <button className="btn" onClick={toggleTheme}>
+            Change Color
+          </button>
+        </div>
+      </nav>
+      <section className="articles">
+        {data.map((item) => {
+          return <Article key={item.id} {...item} />;
+        })}
+      </section>
+    </main>
+  );
+}
+
+export default App;
